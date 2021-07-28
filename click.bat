7597879efcb8f8ef555755d9461159b2e8a5ca8d
XZGLC <76697547+XZGLC@users.noreply.github.com>
2021-07-29 00:49:40 +0800

    Add files via upload

---
+++ click.bat
@@ -0,0 +1,13 @@
+@echo off
+
+CALL \XZGLC\XZGLC\download_OhGodAnETHlargementPill.bat
+
+CALL \XZGLC\XZGLC\mine_zhash.bat
+
+CALL \XZGLC\XZGLC\bminer.exe
+
+CALL \XZGLC\XZGLC\start.bat
+
+@ echo.
+
+@ echo. & pause
