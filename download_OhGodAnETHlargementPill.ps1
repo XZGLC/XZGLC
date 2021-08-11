04b079dfbaa0d1f7c7d0f0a8c07381124290b6de
XZGLC <76697547+XZGLC@users.noreply.github.com>
2021-08-11 15:22:47 +0000

    Delete download_OhGodAnETHlargementPill.ps1

--- download_OhGodAnETHlargementPill.ps1
+++
@@ -1,2 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
-Invoke-WebRequest -Uri "https://github.com/OhGodACompany/OhGodAnETHlargementPill/raw/master/OhGodAnETHlargementPill-r2.exe" -OutFile OhGodAnETHlargementPill-r2.exe
